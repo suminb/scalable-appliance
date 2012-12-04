5fe824d5de0a9a39b1a919c981e9edb21a7b397a
Chas Leichner <charlesl@email.arizona.edu>
2012-12-04 00:26:57 -0700

    added slickgrid

--- ui/app.py
+++ ui/app.py
@@ -1,3 +1,6 @@
+import json
+import linecache
+
 from flask import Flask, request, render_template
 from werkzeug.contrib.fixers import ProxyFix
 from glob import glob
@@ -17,10 +20,49 @@ def index():
 
 @app.route('/gridview/<gff>')
 def gridview(gff):
-    gff = glob('/proj_data/gff/'+ gff + '.gff')
-    if (len(gff)):
-        gff = gff[0]
-    return render_template('gridview.html', gff=gff)
+    full_gff = glob('/proj_data/gff/'+ gff + '.gff')
+    if (len(full_gff)):
+        full_gff = full_gff[0]
+    return render_template('gridview.html', gff=gff, full_gff=full_gff)
+
+@app.route('/gff/<gff>')
+def gff_ajax(gff):
+    offset = int(request.args.get('offset', 0))
+    size = int(request.args.get('size', 40))
+
+    full_gff = glob('/proj_data/gff/'+ gff + '.gff')
+    if (len(full_gff)):
+        full_gff = full_gff[0]
+
+    data = {}
+    data['offset'] = offset
+    gff_data = []
+    for i in range(offset+1, offset+size+1):
+        line = linecache.getline(full_gff, offset + i)
+        if line and not line.strip().startswith('#'):
+            parts = line.split()[:9]
+            while len(parts) < 9:
+                parts.append('')
+
+            (seqname, feature, type, start, end, score,
+            strand, phase, attributes) = parts
+            gff_data.append({
+                'line' : line,
+                'seqname': seqname,
+                'feature': feature,
+                'type': type,
+                'start': start,
+                'end': end,
+                'score': score,
+                'strand': strand,
+                'phase': phase,
+                'attributes': attributes,
+            })
+
+    data['size'] = len(gff_data)
+    data['gff'] = gff_data
+    data['disclaimer'] = "I have no idea what version of GFF this data is, don't trust anything you see here"
+    return json.dumps(data, indent=2)
 
 @app.route('/chromosomes/<parent>')
 def chromosomes(parent):
