43d28ecea668073691cf03873e045c793f51ab37
Evan Briones <erbriones@gmail.com>
2012-12-05 11:11:33 -0700

    Fixed bug in task builder and tweaked maker scripts.

--- backend/deploy/setup-maker.sh
+++ backend/deploy/setup-maker.sh
@@ -6,12 +6,12 @@ rmlib=PReDa_121015_short.fasta
 repeat_protein=TE_protein_db_121015_short_header.fasta
 snaphmm=O.sativa.hmm
 
-maker -CTL
-python set_options.py maker_opts.ctl alttest $alttest
-python set_options.py maker_opts.ctl protein $protein
-python set_options.py maker_opts.ctl rmlib $rmlib
-python set_options.py maker_opts.ctl repeat_protein $repeat_protein
-python set_options.py maker_opts.ctl snaphmm $snaphmm
+maker -CTL -c `grep -c "^processor" /proc/cpuinfo`
+python setoption.py maker_opts.ctl alttest $alttest
+python setoption.py maker_opts.ctl protein $protein
+python setoption.py maker_opts.ctl rmlib $rmlib
+python setoption.py maker_opts.ctl repeat_protein $repeat_protein
+python setoption.py maker_opts.ctl snaphmm $snaphmm
 
 if [[ ! ( -f $alttest && -f $protein && -f $rmlib && -f $snaphmm ) ]]; then
 	cp /home/ubuntu/maker_constant/* .
