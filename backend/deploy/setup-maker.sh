e99773761387a36151b2b3f0967acc9b4dc6a6ca
Evan Briones <erbriones@gmail.com>
2012-12-05 08:13:55 -0700

    Updated deploy scripts and fixed bug with command not being sent.

--- backend/deploy/setup-maker.sh
+++ backend/deploy/setup-maker.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-maker -CTL
-python set_options.py maker_opts.ctl alttest jap_glab_brachy.cdna.fa
-python set_options.py maker_opts.ctl protein jap_glab_brachy.pep.fa 
-python set_options.py maker_opts.ctl rmlib PReDa_121015_short.fasta
-python set_options.py maker_opts.ctl repeat_protein TE_protein_db_121015_short_header.fasta
-python set_options.py maker_opts.ctl snaphmm O.sativa.hmm
+alttest=jap_glab_brachy.cdna.fa
+protein=jap_glab_brachy.pep.fa
+rmlib=PReDa_121015_short.fasta
+repeat_protein=TE_protein_db_121015_short_header.fasta
+snaphmm=O.sativa.hmm
 
-cp /home/ubuntu/maker_constant .
+maker -CTL
+python set_options.py maker_opts.ctl alttest $alttest
+python set_options.py maker_opts.ctl protein $protein
+python set_options.py maker_opts.ctl rmlib $rmlib
+python set_options.py maker_opts.ctl repeat_protein $repeat_protein
+python set_options.py maker_opts.ctl snaphmm $snaphmm
+
+if [[ ! ( -f $alttest && -f $protein && -f $rmlib && -f $snaphmm ) ]]; then
+	cp /home/ubuntu/maker_constant/* .
+else
+	echo "Done"
+fi
