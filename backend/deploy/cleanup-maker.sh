8db7366b40b2007a887f9fd462e02e0e5ecc6612
Evan Briones <erbriones@gmail.com>
2012-12-06 14:29:13 -0700

    Updated scripts.

--- backend/deploy/cleanup-maker.sh
+++ backend/deploy/cleanup-maker.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+name=${WORK_FILE%.*}
+mv $name.maker.output/${name}_datastore/*/*/*.gff $name.gff
+
+sleep 2m
 echo "DONE!"
--- backend/deploy/run-maker.sh
+++ backend/deploy/run-maker.sh
@@ -3,6 +3,4 @@
 file=$1
 name=${file%.*}
 echo $name
-touch $name.gff
-python setoption.py maker_opts.ctl maker_gff $name.gff
 maker -genome $file -nodatastore -base $name --tries 3
