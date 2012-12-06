dbe88fbfb899a5e4fe4700d77c969244e5383819
Chas Leichner <charlesl@email.arizona.edu>
2012-12-06 13:08:22 -0700

    added more verbose output

--- ui/remote.sh
+++ ui/remote.sh
@@ -1,5 +1,7 @@
 sudo service apache2 stop
+echo "Installing dependencies..."
 sudo apt-get install -y git python-pip python-virtualenv python-dev
+echo "Cloning repo..."
 git clone https://github.com/cwvh/scalable-maker.git
 cd scalable-maker/backend/status/
 virtualenv status
@@ -8,4 +10,9 @@ pip install psutil flask
 deactivate
 virtualenv status
 source status/bin/activate
+echo "Starting status server..."
 sudo HOST=0.0.0.0 nohup status/bin/python app.py &
+echo "Starting pong worker..."
+nohup bash pong.sh &
+echo "Done."
+logout
