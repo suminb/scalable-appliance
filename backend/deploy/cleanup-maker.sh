a86b9e212816cec44d8046c645f51200824bdbfe
Evan Briones <erbriones@gmail.com>
2012-12-06 15:26:00 -0700

    Fixed scripts.

--- backend/deploy/cleanup-maker.sh
+++ backend/deploy/cleanup-maker.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-<<<<<<< HEAD
 name=${WORK_FILE%.*}
 mv $name.maker.output/${name}_datastore/*/*/*.gff $name.gff
-
-=======
->>>>>>> 43d28ecea668073691cf03873e045c793f51ab37
+tar czf $name.tar.gz $name.gff
 echo "DONE!"
--- backend/deploy/setup-maker.sh
+++ backend/deploy/setup-maker.sh
@@ -6,22 +6,15 @@ rmlib=PReDa_121015_short.fasta
 repeat_protein=TE_protein_db_121015_short_header.fasta
 snaphmm=O.sativa.hmm
 
-<<<<<<< HEAD
 maker -CTL
-=======
-maker -CTL -c `grep -c "^processor" /proc/cpuinfo`
->>>>>>> 43d28ecea668073691cf03873e045c793f51ab37
 python setoption.py maker_opts.ctl alttest $alttest
 python setoption.py maker_opts.ctl protein $protein
 python setoption.py maker_opts.ctl rmlib $rmlib
 python setoption.py maker_opts.ctl repeat_protein $repeat_protein
 python setoption.py maker_opts.ctl snaphmm $snaphmm
-<<<<<<< HEAD
 python setoption.py maker_opts.ctl genome $WORK_FILE
 python setoption.py maker_opts.ctl cpus `grep -c "^processor" /proc/cpuinfo`
-=======
 
->>>>>>> 43d28ecea668073691cf03873e045c793f51ab37
 if [[ ! ( -f $alttest && -f $protein && -f $rmlib && -f $snaphmm ) ]]; then
 	cp /home/ubuntu/maker_constant/* .
 else
