02f94d0f0bf69e4cdd5493ca6ea5b46df37885ae
Evan Briones <erbriones@gmail.com>
2012-12-09 19:37:09 -0700

    Fix to achieve parallel processing see https://pods.iplantcollaborative.org/wiki/display/ACIC/Suggestions+for+teams

--- backend/deploy/run-maker.sh
+++ backend/deploy/run-maker.sh
@@ -2,4 +2,12 @@
 
 file=$1
 name=${file%.*}
+
+cpus=`grep -c "^processor" /dev/cpuinfo`
+
+for i in $(seq `expr $cpus - 1`);
+do
+    maker -genome $file -nodatastore -base $name --tries 3 &
+done;
+
 maker -genome $file -nodatastore -base $name --tries 3
